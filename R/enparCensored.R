ed0d97669e2de4e7e8232907a7b622d228446a83
SteveMillard <SteveMillard@users.noreply.github.com>
2024-07-06 15:43:21 -0700

    Add files via upload
    Updated versions of enparCensored, enparCensored.km, enparCensored.jackknife, and enparCensored.bootstrap.ci to fix Issue #34.

--- R/enparCensored.R
+++ R/enparCensored.R
@@ -1,14 +1,18 @@
 enparCensored <-
-function (x, censored, censoring.side = "left", correct.se = FALSE, 
-    left.censored.min = "DL", right.censored.max = "DL", ci = FALSE, 
-    ci.method = "normal.approx", ci.type = "two-sided", conf.level = 0.95, 
-    pivot.statistic = "z", ci.sample.size = NULL, n.bootstraps = 1000, seed = NULL) 
+function (x, censored, censoring.side = "left", correct.se = TRUE, 
+    restricted = FALSE, left.censored.min = "Censoring Level", 
+    right.censored.max = "Censoring Level", 
+    ci = FALSE, ci.method = "normal.approx", ci.type = "two-sided", conf.level = 0.95, 
+    pivot.statistic = "t", ci.sample.size = "Total", n.bootstraps = 1000, seed = NULL, 
+    warn = FALSE) 
 {
     if (!is.vector(x, mode = "numeric")) 
         stop("'x' must be a numeric vector")
     if (!is.vector(censored, mode = "numeric") & !is.vector(censored, 
         mode = "logical")) 
         stop("'censored' must be a logical or numeric vector")
+    if (length(x) == 0)
+        stop("'x' has length 0")
     if (length(censored) != length(x)) 
         stop("'censored' must be the same length as 'x'")
     data.name <- deparse(substitute(x))
@@ -17,22 +21,32 @@ function (x, censored, censoring.side = "left", correct.se = FALSE,
         0) {
         x <- x[ok]
         censored <- censored[ok]
-        warning(paste(bad.obs, "observations with NA/NaN/Inf in 'x' and 'censored' removed."))
+        if(warn) {
+            warning(paste(bad.obs, 
+                "observations with NA/NaN/Inf in 'x' and 'censored' removed."))
+        }
     }
+    if (length(x) == 0)
+        stop("'x' has length 0 after omiting observations with NA/NaN/Inf")
     if (any(x <= 0)) 
         stop("All values of 'x' must be positive")
     if (is.numeric(censored)) {
-        if (!all(censored == 0 | censored == 1)) 
+        if (!all(censored == 0 | censored == 1)) {
             stop(paste("When 'censored' is a numeric vector, all values of", 
                 "'censored' must be 0 (not censored) or 1 (censored)."))
+        }
         censored <- as.logical(censored)
     }
     n.cen <- sum(censored)
     if (n.cen == 0) 
-        stop("No censored values indicated by 'censored'.")
+        stop("No censored values indicated by 'censored'.  Use the function enpar()")
     x.no.cen <- x[!censored]
     if (length(unique(x.no.cen)) < 2) 
         stop("'x' must contain at least 2 non-missing, uncensored, distinct values.")
+    if (!is.logical(correct.se) || length(correct.se) != 1)
+        stop("'correct.se' must be a logical scalar")
+    if (!is.logical(restricted) || length(restricted) != 1)
+        stop("'restricted' must be a logical scalar")
     N <- length(x)
     censoring.side <- match.arg(censoring.side, c("left", "right"))
     x.cen <- x[censored]
@@ -40,73 +54,127 @@ function (x, censored, censoring.side = "left", correct.se = FALSE,
     ci.method <- match.arg(ci.method, c("normal.approx", "bootstrap"))
     ci.type <- match.arg(ci.type, c("two-sided", "lower", "upper"))
     pivot.statistic <- match.arg(pivot.statistic, c("z", "t"))
-    if (censoring.side == "left") {
-        dl <- min(cen.levels)
-        if (dl <= min(x.no.cen)) {
-            if (length(left.censored.min) != 1) 
-                stop("The argument 'left.censored.min' must have length 1")
-            if (is.character(left.censored.min)) {
-                if (!(left.censored.min %in% c("DL/2", "DL", 
-                  "Ignore"))) 
-                  stop(paste("When the argument 'left.censored.min' is a", 
-                    "character string, it must equal 'DL', 'DL/2', or 'Ignore'"))
+    ci.sample.size <- match.arg(ci.sample.size, c("Total", "Uncensored"))
+    param.ci.list <- NULL
+    method <- NULL
+    if (restricted) {
+        if (censoring.side == "left") {
+            min.cen.level <- min(cen.levels)
+            if (min.cen.level <= min(x.no.cen)) {
+                if (length(left.censored.min) != 1 || 
+                    (!is.character(left.censored.min) & 
+                    !is.numeric(left.censored.min))) {
+                    stop(paste("The argument 'left.censored.min' must have length 1", 
+                         "and be a character string or a numeric scalar."))
+                }
+                if (is.character(left.censored.min)) {
+                    if (left.censored.min != "Censoring Level") 
+                      stop(paste("When the argument 'left.censored.min' is a", 
+                        "character string, it must equal 'Censoring Level'"))
+                }
+                else {
+                    if (left.censored.min > min.cen.level || left.censored.min <= 0) 
+                      stop(paste("When 'left.censored.min' is a numeric scalar", 
+                        "it must be less than or equal to the smallest", 
+                        "censoring level and greater than 0"))
+                }        
+                method <- "Kaplan-Meier (Restricted Mean)"
+                method <- paste(method, "\n", space(33), 
+                    "Smallest censored value(s)", "\n", space(33),
+                        paste("  set to", left.censored.min), 
+                        sep = "")
+                index.which.cen.and.eq.min.cen.level <- 
+                    which(censored & (x == min.cen.level))
+                if(sum(censored[-index.which.cen.and.eq.min.cen.level]) == 0) {
+                    if(warn) {
+                        warning(paste("No censored observations left after applying", 
+                            "the argument left.censored.min, so enpar() called."))
+                    }
+                    param.ci.list <- enpar(x, ci = ci, ci.method = ci.method, 
+                        ci.type = ci.type, conf.level = conf.level, 
+                        pivot.statistic = pivot.statistic, n.bootstraps = n.bootstraps, 
+                        seed = seed)
+                }
+                else {
+                    if (is.numeric(left.censored.min)) {
+                        x[index.which.cen.and.eq.min.cen.level] <- left.censored.min
+                    }
+                    censored[index.which.cen.and.eq.min.cen.level] <- FALSE
+                }
             }
-            else {
-                if (!is.numeric(left.censored.min) || left.censored.min > 
-                  dl || left.censored.min <= 0) 
-                  stop(paste("When 'left.censored.min' is a numeric scalar", 
-                    "it must be less than or equal to the smallest", 
-                    "censoring limit and greater than 0"))
+        }
+        else {
+            max.cen.level <- max(cen.levels)
+            if (max.cen.level >= max(x.no.cen)) {
+                if (length(right.censored.max) != 1 || 
+                    (!is.character(right.censored.max) & 
+                    !is.numeric(right.censored.max))) {
+                    stop(paste("The argument 'right.censored.max' must have length 1", 
+                        "and be a character string or a numeric scalar."))
+                }
+                if (is.character(right.censored.max)) {
+                    if (right.censored.max != "Censoring Level") 
+                      stop(paste("When the argument 'right.censored.max' is a", 
+                        "character string, it must equal 'Censoring Level'"))
+                }
+                else {
+                    if (right.censored.max < max.cen.level) 
+                      stop(paste("When 'right.censored.max' is a numeric scalar", 
+                        "it must be greater than or equal to the largest", 
+                        "censoring level"))
+                }
+                method <- "Kaplan-Meier (Restricted Mean)"
+                method <- paste(method, "\n", space(33), 
+                    "Largest censored value(s)", "\n", space(33),
+                    paste("  set to", right.censored.max), 
+                    sep = "")
+                index.which.cen.and.eq.max.cen.level <- which(censored & (x == max.cen.level))
+                if(sum(censored[-index.which.cen.and.eq.max.cen.level]) == 0) {
+                    if(warn) {
+                        warning(paste("No censored observations left after applying", 
+                            "the argument right.censored.max, so enpar() called."))
+                    }
+                    param.ci.list <- enpar(x, ci = ci, ci.method = ci.method, 
+                        ci.type = ci.type, conf.level = conf.level, 
+                        pivot.statistic = pivot.statistic, n.bootstraps = n.bootstraps, 
+                        seed = seed)
+                }
+                else {
+                    if (is.numeric(right.censored.max)) {
+                        x[index.which.cen.and.eq.max.cen.level] <- right.censored.max
+                    }
+                    censored[index.which.cen.and.eq.max.cen.level] <- FALSE
+                }
             }
         }
     }
-    else {
-        dl <- max(cen.levels)
-        if (dl >= max(x.no.cen)) {
-            if (length(right.censored.max) != 1) 
-                stop("The argument 'right.censored.max' must have length 1")
-            if (is.character(right.censored.max)) {
-                if (!(right.censored.max %in% c("Ignore", "DL"))) 
-                  stop(paste("When the argument 'right.censored.max'", 
-                    "is a character string, it must equal 'DL' or 'Ignore'"))
-            }
-            else {
-                if (!is.numeric(right.censored.max) || right.censored.max < 
-                  dl) 
-                  stop(paste("When 'right.censored.max' is a numeric scalar", 
-                    "it must be greater than or equal to the largest", 
-                    "censoring limit"))
-            }
+    if(is.null(param.ci.list)) {
+        if(is.null(method)) {
+            method <- "Kaplan-Meier"
+        }
+        if (correct.se) {
+            method <- paste(method, "\n", space(33), "(Bias-corrected se.mean)", 
+                sep = "")
+        }
+        if (!ci || ci.method != "bootstrap") {
+            param.ci.list <- enparCensored.km(x = x, censored = censored, 
+                censoring.side = censoring.side, correct.se = correct.se, 
+                ci = ci, ci.type = ci.type, conf.level = conf.level, 
+                pivot.statistic = pivot.statistic, ci.sample.size = ci.sample.size)
+        }
+        else {
+            param.ci.list <- enparCensored.km(x = x, censored = censored, 
+                censoring.side = censoring.side, correct.se = correct.se, 
+                ci = FALSE)
+            ci.list <- enparCensored.bootstrap.ci(x = x, censored = censored, 
+                censoring.side = censoring.side, correct.se = correct.se, 
+                est.fcn = "enparCensored.km", ci.type = ci.type, 
+                conf.level = conf.level, n.bootstraps = n.bootstraps, 
+                obs.mean = param.ci.list$parameters["mean"], 
+                obs.se.mean = param.ci.list$parameters["se.mean"], seed = seed)
+            param.ci.list <- c(param.ci.list, list(ci.obj = ci.list))
         }
     }
-    if (!ci || ci.method != "bootstrap") {
-        param.ci.list <- enparCensored.km(x = x, censored = censored, 
-            censoring.side = censoring.side, correct.se = correct.se, 
-            left.censored.min = left.censored.min, 
-            right.censored.max = right.censored.max, 
-            ci = ci, ci.type = ci.type, conf.level = conf.level, 
-            pivot.statistic = pivot.statistic, ci.sample.size = ci.sample.size)
-    }
-    else {
-        param.ci.list <- enparCensored.km(x = x, censored = censored, 
-            censoring.side = censoring.side, correct.se = correct.se, 
-            left.censored.min = left.censored.min, 
-            right.censored.max = right.censored.max, 
-            ci = FALSE)
-        ci.list <- enparCensored.bootstrap.ci(x = x, censored = censored, 
-            censoring.side = censoring.side, correct.se = correct.se, 
-            left.censored.min = left.censored.min, 
-            right.censored.max = right.censored.max, 
-            est.fcn = "enparCensored.km", ci.type = ci.type, 
-            conf.level = conf.level, n.bootstraps = n.bootstraps, 
-            obs.mean = param.ci.list$parameters["mean"], 
-            obs.se.mean = param.ci.list$parameters["se.mean"], seed = seed)
-        param.ci.list <- c(param.ci.list, list(ci.obj = ci.list))
-    }
-    method <- "Kaplan-Meier"
-    if (correct.se) 
-        method <- paste(method, "\n", space(33), "(Bias-corrected se.mean)", 
-            sep = "")
     ret.list <- list(distribution = "None", sample.size = N, 
         censoring.side = censoring.side, censoring.levels = cen.levels, 
         percent.censored = (100 * n.cen)/N, parameters = param.ci.list$parameters, 
--- R/enparCensored.km.R
+++ R/enparCensored.km.R
@@ -1,8 +1,6 @@
 enparCensored.km <-
-function (x, censored, censoring.side, correct.se, left.censored.min, 
-    right.censored.max, ci, ci.type, conf.level, pivot.statistic, 
-    ci.sample.size = ifelse(pivot.statistic == "t", n.not.cen, 
-        N)) 
+function (x, censored, censoring.side, correct.se, ci, ci.type, 
+    conf.level, pivot.statistic, ci.sample.size)
 {
     ppoints.list <- ppointsCensored(x = x, censored = censored, 
         censoring.side = censoring.side, prob.method = "kaplan-meier")
@@ -14,54 +12,42 @@ function (x, censored, censoring.side, correct.se, left.censored.min,
     n.not.cen <- N - n.cen
     ord.stats.no.cen <- ord.stats[!Censored]
     F.x.no.cen <- F.x[!Censored]
-    ord.stats.cen <- ord.stats[Censored]
-    y <- ord.stats.no.cen
-    F.y <- F.x.no.cen
-    r <- (1:N)[!Censored][-n.not.cen]
-    ci.ub <- Inf
-    if (censoring.side == "left") {
-        dl <- min(ord.stats.cen)
-        if (dl <= min(ord.stats.no.cen) && left.censored.min != 
-            "Ignore") {
-            index <- match(dl, ord.stats.cen)
-            F.y <- c(F.x[Censored][index], F.y)
-            if (is.numeric(left.censored.min)) {
-                dl <- left.censored.min
-            }
-            else {
-                if (left.censored.min == "DL/2") 
-                  dl <- dl/2
-            }
-            y <- c(dl, y)
-            r <- c(1, r)
-        }
+    count <- rle(ord.stats.no.cen)
+    m <- count$lengths
+    y <- count$values
+    F.y <- F.x.no.cen[match(y, ord.stats.no.cen)]
+    diff.F.y <- diff(c(0, F.y))
+    mu.hat <- sum(y * diff.F.y)
+    sigma.hat <- sqrt(sum((y - mu.hat)^2 * diff.F.y))
+    if(censoring.side == "left") {
+        r <- sapply(y[-1], function(x) sum(ord.stats <= x))
+        F.y.times.diff.y <- F.y[-length(F.y)] * diff(y)
+        A <- cumsum(F.y.times.diff.y)
+        se.muhat <- sqrt(sum(A^2 * m[-1]/(r * (r - m[-1]))))
     }
     else {
-        dl <- max(ord.stats.cen)
-        if (dl >= max(ord.stats.no.cen) && right.censored.max != 
-            "Ignore") {
-            if (is.numeric(right.censored.max)) 
-                dl <- right.censored.max
-            y <- c(y, dl)
-            F.y <- c(F.y, 1)
-            r <- c(r, (1:N)[!Censored][n.not.cen])
-            ci.type <- "lower"
-            ci.ub <- dl
-        }
+        y <- ord.stats.no.cen
+        F.y <- F.x.no.cen
+        r <- (1:N)[!Censored][-n.not.cen]
+        y <- c(0, y)
+        F.y <- c(0, F.y)
+        S.y <- 1 - F.y
+        diff.y <- diff(y)
+        S.y.times.diff.y <- S.y[-length(S.y)] * diff.y
+        A <- rev(cumsum(rev(S.y.times.diff.y[-1])))
+        se.muhat <- sqrt(sum(A^2/((N - r) * (N - r + 1))))
     }
-    y <- c(0, y)
-    F.y <- c(0, F.y)
-    S.y <- 1 - F.y
-    F.y.diffs <- diff(F.y)
-    S.y.times.diff.y <- S.y[-length(S.y)] * diff(y)
-    mu.hat <- sum(S.y.times.diff.y)
-    sigma.hat <- sqrt(sum((y[-1] - mu.hat)^2 * F.y.diffs))
-    A <- rev(cumsum(rev(S.y.times.diff.y[-1])))
-    se.muhat <- sqrt(sum(A^2/((N - r) * (N - r + 1))))
     if (correct.se) 
         se.muhat <- sqrt(n.not.cen/(n.not.cen - 1)) * se.muhat
     parameters <- c(mean = mu.hat, sd = sigma.hat, se.mean = se.muhat)
     if (ci) {
+        ci.ub <- Inf
+        if (pivot.statistic == "z") {
+            ci.sample.size <- N
+        }
+        else {
+          ci.sample.size <- ifelse(ci.sample.size == "Total", N, n.not.cen)
+        }
         ci.obj <- ci.normal.approx(theta.hat = parameters["mean"], 
             sd.theta.hat = parameters["se.mean"], n = ci.sample.size, 
             df = ci.sample.size - 1, ci.type = ci.type, alpha = 1 - 
